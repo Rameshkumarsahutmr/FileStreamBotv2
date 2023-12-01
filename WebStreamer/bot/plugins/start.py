f4d3cbb48b8c5da498ac5d111ae5e69d94fbd76e
Rameshkumarsahutmr <115208196+Rameshkumarsahutmr@users.noreply.github.com>
2023-12-01 23:10:55 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,10 +1,10 @@
 # This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+# Coding: Jyothis Jayanth [@EverythingSuckz]
 
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-from WebStreamer.vars import Var 
+from WebStreamer.vars import Var
 from WebStreamer.bot import StreamBot
 
 @StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
@@ -15,6 +15,20 @@ async def start(_, m: Message):
             Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
             disable_web_page_preview=True, quote=True
         )
+
+    # Your new message
+    message_text = "I am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link."
+
+    # Create buttons with emojis
+    buttons = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("👤 Bot owner", url="https://t.me/being_ram_esh")],
+            [InlineKeyboardButton("💬 Chat support", url="https://t.me/+ZrS7H52Qeik0NjM1")]
+        ]
+    )
+
+    # Reply with the new message and buttons
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        text=message_text,
+        reply_markup=buttons
     )
