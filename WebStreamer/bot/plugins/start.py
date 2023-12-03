9d34eb0620f6ae1a65d788f459af9a8a34ca9ae6
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2023-12-03 18:19:45 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -14,7 +14,26 @@ async def start(_, m: Message):
             Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
             disable_web_page_preview=True, quote=True
         )
-
+      
+    if Var.UPDATES_CHANNEL != "None":        
+        try:
+            user = await m.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+            
+        except UserNotParticipant:
+             await StreamBot.send_photo(
+                chat_id=m.chat.id,
+                photo="https://telegra.ph/file/68259e3c723b935e22e69.jpg",
+                caption="<i>𝙹𝙾𝙸𝙽 CHANNEL 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴🔐</i>",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                
+            )
+             return
     # Your new message
     message_text = "I am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link."
 
